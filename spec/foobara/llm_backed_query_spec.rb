c151d1c6019c22b8510614543b69fa97c596c0d8
Miles Georgi <azimux@gmail.com>
2025-03-04 14:33:20 -0800

    Create ruby project files

---
+++ spec/foobara/llm_backed_query_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Foobara::LlmBackedQuery do
+  it "has a version number" do
+    expect(Foobara::LlmBackedQuery::VERSION).to_not be_nil
+  end
+end
---
+++ src/foobara/llm_backed_query.rb
@@ -0,0 +1,4 @@
+module Foobara
+  module LlmBackedQuery
+  end
+end
