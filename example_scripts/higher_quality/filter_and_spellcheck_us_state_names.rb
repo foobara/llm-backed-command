cc03ca595332391fc9cfa9d6b555be74e3a6830f
Miles Georgi <azimux@gmail.com>
2025-03-16 12:03:20 -0700

    Make higher-quality filter-us-states demo script have a model result

--- example_scripts/higher_quality/filter_and_spellcheck_us_state_names.rb
+++ example_scripts/higher_quality/filter_and_spellcheck_us_state_names.rb
@@ -27,6 +27,10 @@ class VerifiedUsState < Foobara::Model
   end
 end
 
+class SelectedAndCorrectedUsStateNames < Foobara::Model
+  attributes verified: [VerifiedUsState], rejected: [PossibleUsState]
+end
+
 class SelectUsStateNamesAndCorrectTheirSpelling < Foobara::LlmBackedCommand
   description <<~DESCRIPTION
     Accepts a list of possible US state names and sorts them into verified to be the name of a
@@ -37,16 +41,16 @@ class SelectUsStateNamesAndCorrectTheirSpelling < Foobara::LlmBackedCommand
 
     If you pass in ["Kalifornia", "Los Angeles", "New York"] the result will be:
 
-    result[:verified].length # => 2
-    result[:verified][0].possible_us_state.name # => "Kalifornia"
-    result[:verified][0].spelling_correction_required # => true#{" "}
-    result[:verified][0].corrected_spelling # => "California"
-    result[:verified][1].possible_us_state.name # => "New York"
-    result[:verified][1].spelling_correction_required # => false
-    result[:verified][1].corrected_spelling # => nil
+    result.verified.length # => 2
+    result.verified[0].possible_us_state.name # => "Kalifornia"
+    result.verified[0].spelling_correction_required # => true#{" "}
+    result.verified[0].corrected_spelling # => "California"
+    result.verified[1].possible_us_state.name # => "New York"
+    result.verified[1].spelling_correction_required # => false
+    result.verified[1].corrected_spelling # => nil
 
-    result[:rejected].length # => 1
-    result[:rejected][0].name # => "Los Angeles"
+    result.rejected.length # => 1
+    result.rejected[0].name # => "Los Angeles"
   DESCRIPTION
 
   inputs do
@@ -54,10 +58,7 @@ class SelectUsStateNamesAndCorrectTheirSpelling < Foobara::LlmBackedCommand
     llm_model Foobara::Ai::AnswerBot::Types.model_enum, default: "claude-3-7-sonnet-20250219"
   end
 
-  result do
-    verified [VerifiedUsState]
-    rejected [PossibleUsState]
-  end
+  result SelectedAndCorrectedUsStateNames
 end
 
 list_of_possible_us_states = [
@@ -71,17 +72,16 @@ list_of_possible_us_states = [
   PossibleUsState.new(name: "New York")
 ]
 
-command = SelectUsStateNamesAndCorrectTheirSpelling.new(list_of_possible_us_states:)
-result = command.run!
-
 puts "Considering:"
 list_of_possible_us_states.each do |possible_us_state|
   puts "  #{possible_us_state.name}"
 end
 puts
 
-puts "#{result[:verified].length} were verified as US states:"
-result[:verified].each do |verified_us_state|
+result = SelectUsStateNamesAndCorrectTheirSpelling.run!(list_of_possible_us_states:)
+
+puts "#{result.verified.length} were verified as US states:"
+result.verified.each do |verified_us_state|
   puts "  #{verified_us_state.corrected_spelling || verified_us_state.possible_us_state.name}"
   if verified_us_state.spelling_correction_required
     puts "    original incorrect spelling: #{verified_us_state.possible_us_state.name}"
@@ -89,7 +89,7 @@ result[:verified].each do |verified_us_state|
 end
 
 puts
-puts "#{result[:rejected].length} were rejected as non-US states:"
-result[:rejected].each do |rejected_us_state|
+puts "#{result.rejected.length} were rejected as non-US states:"
+result.rejected.each do |rejected_us_state|
   puts "  #{rejected_us_state.name}"
 end
