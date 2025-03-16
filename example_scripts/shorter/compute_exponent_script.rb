c7b3cd3301539c89093531446aba02a118fd3c85
Miles Georgi <azimux@gmail.com>
2025-03-16 12:02:50 -0700

    Shorten example_scripts/shorter as much as is reasonable

--- example_scripts/shorter/compute_exponent_script.rb
+++ example_scripts/shorter/compute_exponent_script.rb
@@ -1,40 +1,13 @@
 #!/usr/bin/env ruby
 
 require "foobara/load_dotenv"
-Foobara::LoadDotenv.run!(env: "development", dir: __dir__)
-
-require "foobara/anthropic_api"
-require "foobara/open_ai_api"
-require "foobara/ollama_api"
+Foobara::LoadDotenv.run!(dir: __dir__)
 
 require "foobara/llm_backed_command"
 
-module Math
-  class ComputeExponent < Foobara::LlmBackedCommand
-    description "Takes an integer base and exponent and returns the base raised to the exponent"
-
-    inputs do
-      base :integer, :required
-      exponent :integer, :required
-      llm_model Foobara::Ai::AnswerBot::Types.model_enum, default: "claude-3-7-sonnet-20250219"
-    end
-
-    result :integer
-  end
+class ComputeExponent < Foobara::LlmBackedCommand
+  inputs base: :integer, exponent: :integer
+  result :integer
 end
 
-llm_model = "claude-3-7-sonnet-20250219"
-# llm_model = "gpt-3.5-turbo"
-# llm_model = "deepseek-r1:32b"
-
-base = 2
-exponent = 4
-
-command = Math::ComputeExponent.new(base:, exponent:, llm_model:)
-outcome = command.run
-
-if outcome.success?
-  puts outcome.result
-else
-  puts outcome.errors_hash
-end
+puts ComputeExponent.run!(base: 2, exponent: 4)
--- example_scripts/shorter/determine_language_client.rb
+++ example_scripts/shorter/determine_language_client.rb
@@ -2,19 +2,6 @@
 
 require "foobara/remote_imports"
 
-Foobara::RemoteImports::ImportCommand.run!(
-  manifest_url: "http://localhost:9292/manifest",
-  to_import: "DetermineLanguage",
-  cache: true
-)
+Foobara::RemoteImports::ImportCommand.run!(manifest_url: "http://localhost:9292/manifest", cache: true)
 
-require "foobara/anthropic_api"
-require "foobara/open_ai_api"
-require "foobara/ollama_api"
-
-code_snippet = "System.out.println"
-llm_model = "claude-3-7-sonnet-20250219"
-# llm_model = "gpt-3.5-turbo"
-# llm_model = "deepseek-r1:32b"
-
-puts DetermineLanguage.run!(llm_model:, code_snippet:)
+puts DetermineLanguage.run!(code_snippet: "System.out.println")
