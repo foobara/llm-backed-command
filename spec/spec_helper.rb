c9892e0d4611adfa560b6f6dba43243463fab21a
Miles Georgi <azimux@gmail.com>
2026-01-02 12:12:03 -0800

    Update VCR config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,6 +22,13 @@ Dir["#{__dir__}/support/**/*.rb"].each { |f| require f }
 
 require "foobara/spec_helpers/all"
 
+# To rerecord this cassette:
+# 1. delete list_models.yml
+# 2. delete tmp/
+# 3. change record: :none to record: :once
+# 4. uncomment the raise below
+# 5. run the test suite
+# 6. undo 3 and 4.
 VCR.use_cassette("list_models", record: :none) do
   require "foobara/anthropic_api"
   require "foobara/ollama_api"
--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,6 +4,17 @@ VCR.configure do |config|
   config.filter_sensitive_data("<SCRUBBED_OPENAI_API_KEY>") { ENV.fetch("OPENAI_API_KEY", nil) }
   config.filter_sensitive_data("<SCRUBBED_ANTHROPIC_API_KEY>") { ENV.fetch("ANTHROPIC_API_KEY", nil) }
   config.filter_sensitive_data("<SCRUBBED_OLLAMA_API_URL>") { ENV.fetch("OLLAMA_API_URL", nil) }
+  config.filter_sensitive_data("<SCRUBBED_OLLAMA_API_KEY>") { ENV.fetch("OLLAMA_API_KEY", nil) }
+
+  config.before_record do |interaction|
+    if interaction.request.headers["Cookie"]
+      interaction.request.headers["Cookie"] = ["<SCRUBBED>"]
+    end
+    if interaction.response.headers["Set-Cookie"]
+      interaction.response.headers["Set-Cookie"] = ["<SCRUBBED>"]
+    end
+  end
+
   config.cassette_library_dir = "spec/vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
