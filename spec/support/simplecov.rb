a0165f068953f23b2c7bc927e2768c944836339f
Miles Georgi <azimux@gmail.com>
2026-01-07 11:32:40 -0800

    Switch to branch coverage
    Co-authored-by: Glauber <glauber.bannwart@gmail.com>

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,9 +1,10 @@
 require "simplecov"
 
 SimpleCov.start do
-  add_filter "spec/support"
-  # enable_coverage :branch
-  minimum_coverage line: 100
-  # TODO: enable this? worth it to get to 100% branch coverage?
-  # minimum_coverage line: 100, branch: 100
+  add_filter "/spec/support/"
+  add_filter "/boot/"
+
+  enable_coverage :branch
+  enable_coverage :line
+  minimum_coverage line: 100, branch: 100
 end
